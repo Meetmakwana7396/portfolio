3a63c1922d469ffa31fc6e95ea7452e8829f1321
Meetmakwana7396 <makwnameet7301@gmail.com>
2023-09-24 15:06:51 +0530

    added some responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import Fade from "@/components/Animation/Fade";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="container max-w-6xl">
-      <section className="mt-40">
+      <section className="pt-40 h-screen">
         <div className="space-y-6">
           <Fade delay={0.3}>
             <div className="text-xl">
@@ -14,14 +13,27 @@ export default function Home() {
           </Fade>
 
           <Fade>
-            <p className="text-[90px] max-w-[750px] leading-snug font-bold">
-              Designing interfaces that{' '}
-              <span className="text-primary tracking-wider">{`"speak"`}</span> the language
-              of users.
+            <p className="text-[50px] md:text-[90px] max-w-[750px] leading-snug font-bold">
+              Designing interfaces that{" "}
+              <span className="text-primary tracking-wider">{`"speak"`}</span>{" "}
+              the language of users.
             </p>
           </Fade>
         </div>
       </section>
+
+      <section className="pt-40 h-screen">
+        <div
+          className=''
+        >
+          <h2 className="text-3xl font-semibold mb-4">About Me</h2>
+          <p className="text-xl leading-relaxed">
+            I'm a creative frontend developer with a passion for crafting
+            stunning and user-friendly web experiences.
+          </p>
+        </div>
+      </section>
+      {/* <Portfolio /> */}
     </div>
   );
 }
