66ca1a30efce9a9432eea9914375132752cc9907
Meetmakwana7396 <makwnameet7301@gmail.com>
2023-09-24 16:34:38 +0530

    SPotify API call

--- utils/spotify.tsx
+++ utils/spotify.tsx
@@ -1,21 +1,19 @@
 // utils/spotify.js
-import axios from "axios";
+import axios from 'axios';
 
-const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
-const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const spotifyClientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
 
 async function getAccessToken() {
-  const authHeader = Buffer.from(
-    `${spotifyClientId}:${spotifyClientSecret}`
-  ).toString("base64");
+  const authHeader = Buffer.from(`${spotifyClientId}:${spotifyClientSecret}`).toString('base64');
 
   const response = await axios.post(
-    "https://accounts.spotify.com/api/token",
-    "grant_type=client_credentials",
+    'https://accounts.spotify.com/api/token',
+    'grant_type=client_credentials',
     {
       headers: {
         Authorization: `Basic ${authHeader}`,
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
     }
   );
@@ -26,9 +24,9 @@ async function getAccessToken() {
 async function getRecentTracks() {
   try {
     const accessToken = await getAccessToken();
-
+    
     const response = await axios.get(
-      "https://api.spotify.com/v1/me/player/recently-played?limit=5",
+      'https://api.spotify.com/v1/me/player/recently-played?limit=5', // Fetch the recent 5 tracks
       {
         headers: {
           Authorization: `Bearer ${accessToken}`,
@@ -37,7 +35,10 @@ async function getRecentTracks() {
     );
 
     return response.data.items;
-  } catch (error) {}
+  } catch (error) {
+    console.error('Error fetching recent tracks:', error);
+    return []; // Return an empty array or handle the error as needed
+  }
 }
 
 export { getRecentTracks };
